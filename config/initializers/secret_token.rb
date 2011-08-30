42f15bfd5711aa35548357f2005b2974421d8f64
koos <koos42@gmail>
2011-08-30 00:01:02 -0700

    blank-ass rails site committed...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KoozyCollection::Application.config.secret_token = '697ce352b6ef72829b7892ca55ad7d1cb4e13fd8385918d30f1bd02094c785b2eb50102cc2ccce56d9de25deba10f651367a228366e9b87f42500cb74abde8fd'
